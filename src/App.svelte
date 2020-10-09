4454f03391f71a4c7b155256393d32087427b20b
Simon Nyström <simon.nystrom90@gmail.com>
2020-10-09 18:46:42 +0200

    Part 3

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,32 @@
 <script>
-  import Input from "./components/Input.svelte";
-  let value = "Initial input";
+  import Form from "./components/Form.svelte";
+
+  //   Our field representation, let's us easily specify several inputs
+  let fields = [
+    {
+      name: "firstName",
+      type: "Input",
+      value: "",
+      placeholder: "Enter first name...",
+      label: "First name",
+    },
+    {
+      name: "lastName",
+      type: "Input",
+      value: "",
+      placeholder: "Enter last name...",
+      label: "Last name",
+    },
+  ];
+
+  let result = {};
 </script>
 
-<main>
-  <Input
-    bind:value
-    name="name"
-    label="Name"
-    id="name"
-    placeholder="Enter name..." />
-</main>
+<Form
+  onSubmit={(body) => {
+    result = body;
+  }}
+  {fields} />
+
+<!-- Show the current state of our Form -->
+<pre><code>{JSON.stringify(result, 0, 2)}</code></pre>
---
+++ src/components/Form.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Input from "./Input.svelte";
+
+  export let onSubmit;
+  export let fields;
+
+  // Convert fields from [ { name: 'name', value: 'Value' } ] to { name : Value } which is more useful when submitting a form
+  const fieldsToObject = (fields) =>
+    fields.reduce((p, c) => ({ ...p, [c.name]: c.value }), {});
+
+  // When submitting, turn our fields representation into a JSON body
+  const handleSubmit = () => onSubmit(fieldsToObject(fields));
+</script>
+
+<style>
+  :global(input, select) {
+    margin: 5px;
+  }
+</style>
+
+<!-- When submitting, prevent the default action which would result in a refreshed page -->
+<form on:submit|preventDefault={() => handleSubmit(fields)}>
+    <!-- Loop the fields and render the correct representation based on field.type -->
+    {#each fields as field}
+        {#if field.type === 'Input'}
+        <Input bind:value={field.value} label={field.label} placeholder={field.placeholder} />
+        {/if}
+    {/each}
+    <button type="submit">Submit</button>
+</form>
